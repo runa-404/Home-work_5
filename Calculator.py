06812abc1da5bf6f27618671a190b9b4e6105153
ㄕㄩㄉㄊ <lomynohaihnatii@gmail.com>
2025-07-14 14:11:04 +0300

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+
+import my_math
+
+x = 10
+y = 5
+
+print("Додавання:", my_math.add(x, y))
+print("Віднімання:", my_math.subtract(x, y))
+print("Множення:", my_math.multiply(x, y))
+print("Ділення:", my_math.divide(x, y))
